9b1dbea5d68310d158facdbf50c72ca9dc6f9dbd
Sergey Sporshev <zxcser00@gmail.com>
2024-01-14 14:09:16 +0300

    lab2: Многоклассовая классификация

--- lab2_sgd/lab2_mlp_batchgradient.py
+++ lab2_sgd/lab2_mlp_batchgradient.py
@@ -11,16 +11,14 @@ import pandas as pd
 
 df = pd.read_csv('data.csv')
 
-# возьмем перые 100 строк, 4-й столбец 
-y = df.iloc[0:100, 4].values
-# так как ответы у нас строки - нужно перейти к численным значениям
-y = np.where(y == "Iris-setosa", 1, 0).reshape(-1,1) # reshape нужен для матричных операций
+y = df.iloc[0:150, 4].values
+# Переводим строковые метки классов в численные значения
+class_mapping = {"Iris-setosa": 0, "Iris-versicolor": 1, "Iris-virginica": 2}
+y = np.array([class_mapping[label] for label in y])
+# Преобразование вектора меток в матрицу one-hot encoding
+y_one_hot = np.eye(3)[y]
 
-# возьмем два признака, чтобы было удобне визуализировать задачу
-X = df.iloc[0:100, [0, 2]].values
-
-# добавим фиктивный признак для удобства матричных вычслений
-X = np.concatenate([np.ones((len(X),1)), X], axis = 1)
+X = df.iloc[0:150, 0:4].values
 
 # зададим функцию активации - сигмоида
 def sigmoid(y):
@@ -31,10 +29,20 @@ def derivative_sigmoid(y):
     return sigmoid(y) * (1 - sigmoid(y))
 
 
+# Софтмакс функция активации для многоклассовой классификации
+def softmax(x):
+    exp_values = np.exp(x - np.max(x, axis=1, keepdims=True))
+    return exp_values / np.sum(exp_values, axis=1, keepdims=True)
+
+# Производная софтмакса
+def derivative_softmax(x):
+    return x * (1 - x)
+
+
 # инициализируем нейронную сеть 
 inputSize = X.shape[1] # количество входных сигналов равно количеству признаков задачи 
 hiddenSizes = 5 # задаем число нейронов скрытого слоя 
-outputSize = 1 if len(y.shape) else y.shape[1] # количество выходных сигналов равно количеству классов задачи
+outputSize = 3 # количество выходных сигналов равно количеству классов задачи
 
 # веса инициализируем случайными числами, но теперь будем хранить их списком
 weights = [
@@ -46,7 +54,7 @@ weights = [
 def feed_forward(x):
     input_ = x # входные сигналы
     hidden_ = sigmoid(np.dot(input_, weights[0])) # выход скрытого слоя = сигмоида(входные сигналы*веса скрытого слоя)
-    output_ = sigmoid(np.dot(hidden_, weights[1]))# выход сети (последнего слоя) = сигмоида(выход скрытого слоя*веса выходного слоя)
+    output_ = softmax(np.dot(hidden_, weights[1]))# выход сети (последнего слоя) = сигмоида(выход скрытого слоя*веса выходного слоя)
 
     # возвращаем все выходы, они нам понадобятся при обратном проходе
     return [input_, hidden_, output_]
@@ -65,7 +73,7 @@ def backward(learning_rate, target, net_output, layers):
         # градиент слоя = ошибка слоя * производную функции активации * на входные сигналы слоя
         
         # ошибка слоя * производную функции активации
-        err_delta = err * derivative_sigmoid(layers[i])       
+        err_delta = err * derivative_softmax(layers[i])   
         
         # пробрасываем ошибку на предыдущий слой
         err = np.dot(err_delta, weights[i - 1].T)
@@ -95,31 +103,31 @@ def train_stochastic(x_values, target, learning_rate):
 
 # функция предсказания возвращает только выход последнего слоя
 def predict(x_values):
-    return feed_forward(x_values)[-1]
+    return np.argmax(feed_forward(x_values)[-1], axis=1)
 
 
 # задаем параметры обучения
-iterations = 50
+iterations = 500
 learning_rate = 0.01
 
 # обучаем сеть (фактически сеть это вектор весов weights)
 for i in range(iterations):
-    train_stochastic(X, y, learning_rate)
+    train_stochastic(X, y_one_hot, learning_rate)
 
     if i % 10 == 0:
-        print("На итерации: " + str(i) + ' || ' + "Средняя ошибка: " + str(np.mean(np.square(y - predict(X)))))
+        accuracy = np.mean(predict(X) == y)
+        print("На итерации: " + str(i) + ' || ' + "Точность: " + str(accuracy))
 
 # считаем ошибку на обучающей выборке
 pr = predict(X)
-print(sum(abs(y-(pr>0.5))))
+print(sum(abs(y-(pr))))
 
 
 # считаем ошибку на всей выборке
 y = df.iloc[:, 4].values
-y = np.where(y == "Iris-setosa", 1, 0).reshape(-1,1) 
-X = df.iloc[:, [0, 2]].values
-X = np.concatenate([np.ones((len(X),1)), X], axis = 1)
+y = np.array([class_mapping[label] for label in y])
+X = df.iloc[:, 0:4].values
 
 pr = predict(X)
-print(sum(abs(y-(pr>0.5))))
-
+print(sum(abs(y-(pr))))
+print(list(zip(y, pr)))
